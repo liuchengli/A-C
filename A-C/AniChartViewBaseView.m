5fcecb46b0c2b615f83df1addf36d99977fe5342
刘成利 <liuchengli@192.169.20.170.static.etheric.net>
2017-02-10 17:46:20 +0800

    optimize present colors

--- A-C/AniChartViewBaseView.m
+++ A-C/AniChartViewBaseView.m
@@ -11,6 +11,8 @@
 
 @property (nonatomic, strong) NSMutableArray  *shapeLayerArray;  // 存储每条柱
 
+@property (nonatomic, strong) NSArray         *colorsArray;  // 颜色集合
+
 @end
 
 
@@ -24,6 +26,21 @@
     {
         self.shapeLayerArray  = [NSMutableArray array];
         
+        self.colorsArray = @[[UIColor colorWithRed:223/255.0 green:73/255.0 blue:53/255.0 alpha:1],
+          [UIColor colorWithRed:243/255.0 green:143/255.0 blue:43/255.0 alpha:1],
+          [UIColor colorWithRed:235/255.0 green:218/255.0 blue:89/255.0 alpha:1],
+          [UIColor colorWithRed:115/255.0 green:216/255.0 blue:75/255.0 alpha:1],
+          [UIColor colorWithRed:58/255.0 green:223/255.0 blue:210/255.0 alpha:1],
+          [UIColor colorWithRed:32/255.0 green:172/255.0 blue:255.0/255.0 alpha:1],
+          [UIColor colorWithRed:39/255.0 green:92/255.0 blue:170/255.0 alpha:1],
+                             [UIColor colorWithRed:141/255.0 green:132/255.0 blue:240/255.0 alpha:1],[UIColor colorWithRed:223/255.0 green:73/255.0 blue:53/255.0 alpha:1],
+                             [UIColor colorWithRed:243/255.0 green:143/255.0 blue:43/255.0 alpha:1],
+                             [UIColor colorWithRed:235/255.0 green:218/255.0 blue:89/255.0 alpha:1],
+                             [UIColor colorWithRed:115/255.0 green:216/255.0 blue:75/255.0 alpha:1],
+                             [UIColor colorWithRed:58/255.0 green:223/255.0 blue:210/255.0 alpha:1],
+                             [UIColor colorWithRed:32/255.0 green:172/255.0 blue:255.0/255.0 alpha:1],
+                             [UIColor colorWithRed:39/255.0 green:92/255.0 blue:170/255.0 alpha:1],
+                             [UIColor colorWithRed:141/255.0 green:132/255.0 blue:240/255.0 alpha:1]];
         
     }
     return self;
@@ -73,7 +90,9 @@
         // 4.4创建柱状型的柱子
         
         UIView *barView = [[UIView alloc]initWithFrame:CGRectMake(barX, self.frame.size.height, barWith,0)];
-        barView.backgroundColor =[UIColor colorWithRed:arc4random_uniform(256) /255.0 green:arc4random_uniform(256) /255.0 blue:arc4random_uniform(256) /255.0 alpha:0.8];
+//        barView.backgroundColor =[UIColor colorWithRed:arc4random_uniform(256) /255.0 green:arc4random_uniform(256) /255.0 blue:arc4random_uniform(256) /255.0 alpha:0.8];
+        
+        barView.backgroundColor  = self.colorsArray[i];
         [self addSubview:barView];
        
         
