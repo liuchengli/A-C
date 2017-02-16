2e850b9323ea7fdfd5a968a7dc08f3f190f7a94a
刘成利 <liuchengli@192.169.20.170.static.etheric.net>
2017-02-16 15:59:40 +0800

    add long press button

--- A-C/AnimationsListViewController.m
+++ A-C/AnimationsListViewController.m
@@ -32,6 +32,7 @@
 #import "DrawMarqueeViewController.h"
 #import "MixedColorProgressViewController.h"
 #import "BlurryViewController.h"
+#import "LongPressButtonViewController.h"
 #import "AlertController.h"
 #import "MaskViewController.h"
 
@@ -230,6 +231,7 @@
                        [Item itemWithName:@"复合缩放文字" object:[LabelScaleViewController class]],
                        [Item itemWithName:@"跑马灯文字" object:[DrawMarqueeViewController class]],
                        [Item itemWithName:@"底色混色文字" object:[MixedColorProgressViewController class]],
+                        [Item itemWithName:@"长按按钮" object:[LongPressButtonViewController class]],
                        [Item itemWithName:@"view模糊" object:[BlurryViewController class]],
                        [Item itemWithName:@"弹窗" object:[AlertController class]],
                        [Item itemWithName:@"蒙版引导" object:[MaskViewController class]]
---
+++ A-C/LongPressButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  LongPressButtonViewController.h
+//  A-C
+//
+//  Created by 刘成利 on 2017/2/16.
+//  Copyright © 2017年 刘成利. All rights reserved.
+//
+
+#import "NormalTitleViewController.h"
+
+@interface LongPressButtonViewController : NormalTitleViewController
+
+@end
