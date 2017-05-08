36df24ada8643cf65d1282e2ae7f218cab41ea2c
刘成利 <liu_cheng_li@qq.com>
2017-05-08 22:23:44 +0800

    add string

--- A-C/TextGradualChangeController.m
+++ A-C/TextGradualChangeController.m
@@ -26,7 +26,7 @@
     self.fadeInView           = [[LazyFadeInView alloc] initWithFrame:CGRectMake(20, 20, Width - 40, self.contentView.height - 40)];
     self.fadeInView.delegate  = self;
     self.fadeInView.textColor = [UIColor whiteColor];
-    self.fadeInView.text      = @"夫君子之行，静以修身，俭以养德。非淡泊无以明志，非宁静无以致远。夫学须静也，才须学也。非学无以广才，非志无以成学。韬慢则不能励精，险躁则不能冶性。年与时驰，意与日去，遂成枯落，多不接世。悲守穷庐，将复何及？";
+    self.fadeInView.text      = @"夫君子之行，静以修身，俭以养德。非淡泊无以明志，非宁静无以致远。夫学须静也，才须学也。非学无以广才，非志无以成学。韬慢则不能励精，险躁则不能冶性。年与时驰，意与日去，遂成枯落，多不接世。悲守穷庐，将复何及？\n\n卿生于苏杭，我葬身北邙；\n卿说江南好风光，我却长眠在洛阳。\n卿喜舞霓裳，我厌做帝皇；\n卿说梅雨过潇湘，我却哀怨世道凉。\n\n卿生于苏杭，我葬身北邙；\n卿说挥军进咸阳，我便火烧宫阿房。\n卿喜着男装，我为楚霸王；\n卿说奈何弃娇娘，我便自刎绝乌江。\n\n卿生于苏杭，我葬身北邙；\n卿说家住水墨庄，我却骨枯已沦亡。\n卿喜点粉妆，我放逸情狂；\n卿说唇抹胭脂烫，我却长吻把命丧。\n\n卿生于苏杭，我葬身北邙；\n卿说蝴蝶影成双，我便断翼同翱翔。\n卿喜怒无常，我风流倜傥；\n卿说花败亦非伤，我便残红染泥香。";
     self.fadeInView.textFont  = [UIFont HeitiSCWithFontSize:14.f];
     [self.contentView addSubview:self.fadeInView];
 }
